aeca31d164575f9b75ca143caaaff2b7b6edf3cd
LIJU SAMUEL <liju13samuel@gmail.com>
2024-03-09 15:52:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const dateTime = '2024-03-09 12:00:00';
 const fromTimeZone = 'America/New_York';
 const format = 'YYYY-MM-DD HH:mm:ss';
 
-const allTimeZones = convertToAllTimeZones(dateTime, fromTimeZone, format); // Use the imported functions directly
+const allTimeZones = convertToAllTimeZones(dateTime, fromTimeZone, format); 
 console.log('Converted times for all time zones:', allTimeZones);
